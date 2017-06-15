172d84183d55ea5fc74aac9c16827f3b0f560c42
amitaibu <amitai@gizra.com>
2017-06-15 13:20:21 +0300

    Add yesod settings

--- server/src/Handler/Home.hs
+++ server/src/Handler/Home.hs
@@ -6,11 +6,22 @@
 
 module Handler.Home where
 
+import Data.Aeson.Text (encodeToLazyText)
 import Import
+import Text.Julius (rawJS)
+
+data JsSettings = JsSettings
+    { foo :: Text
+    }
+
+instance ToJSON JsSettings where
+    toJSON jsSettings = object ["foo" .= foo jsSettings]
 
 getHomeR :: Handler Html
-getHomeR =
+getHomeR = do
+    let jsSettings = encodeToLazyText JsSettings {foo = "bar"}
     defaultLayout $ do
         setTitle "Welcome To SeemsSo!"
+        $(widgetFile "js-settings")
         addScript $ StaticR js_Main_js
         addScript $ StaticR js_app_js
