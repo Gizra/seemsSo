5d5a55582cef9cc62b997015565183c26df17c8d
Amitai Burstein <amitai@gizra.com>
2017-06-21 22:04:15 +0300

    Use MaybeT for hasAccessToPdfFileDownload

--- server/src/Utils/Order.hs
+++ server/src/Utils/Order.hs
@@ -6,6 +6,7 @@
 
 module Utils.Order where
 
+import Control.Monad.Trans.Maybe
 import Import.NoFoundation
 import Model.Types
 
@@ -21,36 +22,32 @@ hasAccessToPdfFileDownload ::
     -> HandlerT site IO AuthResult
 hasAccessToPdfFileDownload userId filename = do
     let unauthorized = Unauthorized "You didn't buy this item"
-    mpdf <- runDB $ selectFirst [PdfFileFilename ==. unpack filename] []
-    case mpdf of
-        Nothing -> return unauthorized
-        Just (Entity pdfId _) -> do
-            mitem <- runDB $ selectFirst [ItemPdfFile ==. Just pdfId] []
+    result <-
+        runMaybeT $ do
+            (Entity pdfId _) <-
+                maybeTselectFirst [PdfFileFilename ==. unpack filename]
             -- Find the item that references the PDF.
-            case mitem of
-                Nothing -> return unauthorized
-                Just (Entity itemId _) -> do
-                    morderItem <-
-                        runDB $
-                        selectFirst
-                            [OrderItemItem ==. itemId, OrderItemUser ==. userId]
-                            []
-                    -- Find the OrderItem that references the Item, that belongs to the user.
-                    case morderItem of
-                        Nothing -> return unauthorized
-                        Just (Entity _ orderItem) -> do
-                            morder <-
-                                runDB $
-                                selectFirst
-                                    [OrderId ==. orderItemOrder orderItem]
-                                    []
-                            -- Find the Order that the order item belongs to.
-                            case morder of
-                                Nothing -> return unauthorized
-                                Just (Entity _ order)
-                                -- Validate it has a "paid" status.
-                                 ->
-                                    return $
-                                    if orderStatus order == OrderStatusPaid
-                                        then Authorized
-                                        else unauthorized
+            (Entity itemId _) <- maybeTselectFirst [ItemPdfFile ==. Just pdfId]
+            -- Find the OrderItem that references the Item, that belongs to the user.
+            (Entity _ orderItem) <-
+                maybeTselectFirst
+                    [OrderItemItem ==. itemId, OrderItemUser ==. userId]
+            -- Find the Order that the order item belongs to.
+            (Entity _ order) <-
+                maybeTselectFirst [OrderId ==. orderItemOrder orderItem]
+            -- Validate it has a "paid" status.
+            if orderStatus order == OrderStatusPaid
+                then
+                  return Authorized
+                else return unauthorized
+    return $ maybe unauthorized id result
+
+maybeTselectFirst ::
+       ( PersistEntityBackend record ~ BaseBackend (YesodPersistBackend site)
+       , PersistEntity record
+       , PersistQueryRead (YesodPersistBackend site)
+       , YesodPersist site
+       )
+    => [Filter record]
+    -> MaybeT (HandlerT site IO) (Entity record)
+maybeTselectFirst selectOps = MaybeT $ runDB $ selectFirst selectOps []
