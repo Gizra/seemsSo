76a65b5a15dc7adae418f9ca4fbf71f81889e7db
amitaibu <amitai@gizra.com>
2017-06-28 12:08:53 +0300

    Add more tests

--- server/test/Handler/CommentSpec.hs
+++ server/test/Handler/CommentSpec.hs
@@ -8,20 +8,35 @@ module Handler.CommentSpec
 import TestImport
 
 spec :: Spec
-spec = do
+spec =
     withApp $
-        describe "Item's comments" $ do
-            it "should show 'No comments' to anonymous user" $ do
-                (_, _, _, itemId) <- prepareScenario
-                get $ ItemR itemId
-                htmlAnyContain "section.comments > div" "No comments"
-            it "should show 'No comments' to authenticated user" $ do
-                (_, _, _, itemId) <- prepareScenario
-                alice <- createUser "alice"
-                authenticateAs alice
-                get $ ItemR itemId
-                htmlAnyContain "section.comments > div" "No comments"
-            
+    describe "Item's comments" $ do
+        it "should show 'No comments' to anonymous user" $ do
+            (_, _, _, itemId) <- prepareScenario
+            get $ ItemR itemId
+            htmlAnyContain "section.comments > div" "No comments"
+        it "should show 'No comments' to authenticated user" $ do
+            (_, _, _, itemId) <- prepareScenario
+            alice <- createUser "alice"
+            authenticateAs alice
+            get $ ItemR itemId
+            htmlAnyContain "section.comments > div" "No comments"
+        it "should show comment to anonymous user" $ do
+            (john, _, _, itemId) <- prepareScenario
+            let (Entity userId _) = john
+            let commentText = "comment added by John"
+            currentTime <- liftIO getCurrentTime
+            _ <-
+                runDB $
+                insert $ ItemComment commentText itemId userId currentTime
+            get $ ItemR itemId
+            htmlAnyContain ".ui.comments > .comment > div > div.author" "john"
+            htmlAnyContain
+                ".ui.comments > .comment > div > div.text"
+                (unpack commentText)
+
+
+
 
 prepareScenario :: YesodExample App (Entity User, CompanyId, PdfFileId, ItemId)
 prepareScenario = do
