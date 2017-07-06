1c586e58f4c7f9be2a5a71608dc23d41706b111f
amitaibu <amitai@gizra.com>
2017-07-06 13:58:08 +0300

    Add RESTful tests

--- server/test/Handler/ItemCommentSpec.hs
+++ server/test/Handler/ItemCommentSpec.hs
@@ -9,30 +9,55 @@ import TestImport
 
 spec :: Spec
 spec =
-    withApp $
-    describe "Item's comments" $ do
-        it "should show 'No comments' to anonymous user" $ do
-            (_, _, _, itemId) <- prepareScenarioWithoutComment
-            get $ ItemR itemId
-            htmlAnyContain "section.comments > div" "No comments"
-        it "should show 'No comments' to authenticated user" $ do
-            (_, _, _, itemId) <- prepareScenarioWithoutComment
-            bob <- createUser "bob"
-            authenticateAs bob
-            get $ ItemR itemId
-            htmlAnyContain "section.comments > div" "No comments"
-        it "should show comment to anonymous user" $ do
-            (_, _, _, itemId, _, _) <- prepareScenario
-            assertCommentExists itemId
-        it "should show comment to autheenticated user" $ do
-            (_, _, _, itemId, _, _) <- prepareScenario
-            bob <- createUser "bob"
-            authenticateAs bob
-            assertCommentExists itemId
-        it "should show comment to comment owner" $ do
-            (_, _, _, itemId, alice, _) <- prepareScenario
-            authenticateAs alice
-            assertCommentExists itemId
+    withApp $ do
+        describe "Item's comments" $ do
+            it "should show 'No comments' to anonymous user" $ do
+                (_, _, _, itemId) <- prepareScenarioWithoutComment
+                get $ ItemR itemId
+                htmlAnyContain "section.comments > div" "No comments"
+            it "should show 'No comments' to authenticated user" $ do
+                (_, _, _, itemId) <- prepareScenarioWithoutComment
+                bob <- createUser "bob"
+                authenticateAs bob
+                get $ ItemR itemId
+                htmlAnyContain "section.comments > div" "No comments"
+            it "should show comment to anonymous user" $ do
+                (_, _, _, itemId, _, _) <- prepareScenario
+                assertCommentExists itemId
+            it "should show comment to autheenticated user" $ do
+                (_, _, _, itemId, _, _) <- prepareScenario
+                bob <- createUser "bob"
+                authenticateAs bob
+                assertCommentExists itemId
+            it "should show comment to comment owner" $ do
+                (_, _, _, itemId, alice, _) <- prepareScenario
+                authenticateAs alice
+                assertCommentExists itemId
+        describe "Item's comments via RESTful" $ do
+            it
+                "should show allow access to anonymous user when there are no comments" $ do
+                (_, _, _, itemId) <- prepareScenarioWithoutComment
+                get $ RestfulItemCommentsR itemId
+                statusIs 200
+            it
+                "should show allow access to anonymous user when there are comments" $ do
+                (_, _, _, itemId, _, _) <- prepareScenario
+                get $ RestfulItemCommentsR itemId
+                statusIs 200
+            it
+                "should show allow access to authenticated user when there are no comments" $ do
+                (_, _, _, itemId) <- prepareScenarioWithoutComment
+                bob <- createUser "bob"
+                authenticateAs bob
+                get $ RestfulItemCommentsR itemId
+                statusIs 200
+            it
+                "should show allow access to authenticated user when there are comments" $ do
+                (_, _, _, itemId, _, _) <- prepareScenario
+                bob <- createUser "bob"
+                authenticateAs bob
+                get $ RestfulItemCommentsR itemId
+                statusIs 200
 
 {-| Go to Item's page, and assert the comment exist.
 -}
