a26825d91d536949ddb18ebcfe155958b81e853e
Amitai Burstein <amitai@gizra.com>
2017-07-19 22:31:00 +0300

    Don't show comment form to anon user

--- client/src/elm/Pages/Item/View.elm
+++ client/src/elm/Pages/Item/View.elm
@@ -5,6 +5,7 @@ import Html exposing (..)
 import Html.Attributes exposing (alt, class, classList, href, placeholder, src, style, target, type_, value)
 import Html.Events exposing (onClick, onInput)
 import ItemComment.View exposing (viewItemComments)
+import Maybe.Extra exposing (isJust)
 import Pages.Item.Model exposing (Model, Msg(..))
 import User.Model exposing (User)
 import Utils.Html exposing (divider, sectionDivider, showIf, showMaybe)
@@ -14,5 +15,5 @@ view : String -> Maybe User -> Model -> Html Msg
 view baseUrl muser model =
     div []
         [ viewItemComments muser model.comments
-        , Html.map MsgItemComment <| ItemComment.View.view baseUrl muser model.itemComment
+        , showIf (isJust muser) <| Html.map MsgItemComment <| ItemComment.View.view baseUrl muser model.itemComment
         ]
